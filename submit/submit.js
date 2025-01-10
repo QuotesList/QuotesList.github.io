b97f2559c66e37d2199b4faa1db8427e10b65a2b
Adam Seidman <adamjseidman@gmail.com>
2025-01-09 22:29:15 -0500

    Add more 'submit' functionality

--- submit/submit.js
+++ submit/submit.js
@@ -80,6 +80,44 @@ const suggestAuthors = (isGroup, quote) => {
     }
 }
 
+var escapeText = function escape (htmlStr) { 
+    return htmlStr.replace(/&/g, "&amp;")
+          .replace(/</g, "&lt;")
+          .replace(/>/g, "&gt;")
+          .replace(/"/g, "&quot;")
+          .replace(/'/g, "&#39;");        
+
+}
+
+const updateAuthorList = authorList => {
+    if (!Array.isArray(authorList)) {
+        return
+    }
+    let authors = ''
+    authorList.forEach((author, idx) => {
+        if (Array.isArray(author)) {
+            author = author[0]
+            // TODO This means its a new author
+        }
+        authors += `<li class="clearfix"><span class="authorName">${author}&ensp;</span>`
+        authors += `<div class="name-btn-div float-right"><button class="name-btn name-edit-btn" data-toggle="modal" dataTarget="#editModal${idx}"> \
+            <i class="fa fa-pencil" aria-hidden="true"></i></button> \
+            <button class="name-btn name-delete-btn" onclick="deleteName('${author}')"> \
+            <i class="fa fa-trash" aria-hidden="true"></i></button></li></div>\n`
+        authors += `<div id=editModal${idx}" class="modal fade" role="dialog"><div class="modal-dialog"> \
+            <div class="modal-content"><div class="modal-header"><h4 class="modal-title">Edit</h4> \
+            <button type="button" class="close" data-dismiss="modal">&times;</button></div> \
+            <div class="modal-body"><span>TEST_CONTENT</span><br> \
+            <button type="button" class="btn btn-default float-right" id="edit${idx}" data-dismiss="modal" onclick="editName(this, ${author})">Save</button> \
+            </div></div></div></div>\n`
+    })
+    authors += '<li><input id="add-name"> \
+        <button class="name-btn name-input-btn float-right" id="add-name" data-toggle="modal" data-target="#addModal" onclick="openAddModal()"> \
+        <i class="fa fa-plus" aria-hidden="true"></i></button></li>\n'
+    // TODO: authors += (add modal)
+    document.getElementById('authorList').innerHTML = authors
+}
+
 const submitParseForm = () => {
     document.getElementById('parse-btn').disabled = true
     let quote = document.getElementById('qtext').value
@@ -88,17 +126,29 @@ const submitParseForm = () => {
         return
     }
     let isGroup = isGroupQuote(quote)
-    if (isGroup === undefined) {
+    if (typeof isGroup != 'boolean') {
         alert('Could not read quote!')
         return
     }
     let authors = suggestAuthors(isGroup, quote)
     getNameGuesses(authors)
         .then(suggestions => {
-            console.log(suggestions) // TODO generate modal
+            document.getElementById('submit-btn').disabled = false
+            document.getElementById('quoteToSubmit').innerHTML = escapeText(quote).replaceAll('\n', '<br>')
+            document.getElementById('isGroupCheckbox').checked = isGroup
+            updateAuthorList(Object.values(suggestions))
+            $('#submitModal').show()
         })
         .catch(err => {
             console.error('Could not get search results.', err)
             alert('Could not get speaker suggestions!')
         })
+}
+
+const submitQuoteForm = () => {
+    document.getElementById('submit-btn').disabled = true
+    $('#submitModal').hide()
+    document.getElementById('qtext').value = ""
+    alert('Quote Submitted! (kind of)') // TODO
+    document.getElementById('parse-btn').disabled = false
 }
