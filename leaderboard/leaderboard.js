ca67d0e35cb1ac17d7287139413d1cf09263f7e1
Adam Seidman <adamjseidman@gmail.com>
2025-02-15 00:18:49 -0500

    Add tooltip to quote button

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -198,7 +198,7 @@ getAllQuotes(true)
                                     </button>
                                 </div>
                                 <div class="modal-body">
-                                    <i class="fa fa-comment float-right" data-name="${person}" aria-hidden="true"></i>
+                                    <i class="fa fa-comment float-right" data-name="${person}" aria-hidden="true" title="View ${person}'s Quotes"></i>
                                     <h5>Number of Quotes: ${stats.numQuotes}</h5>
                                     <h5 class="mobile-only">Numer of Solo Quotes: ${stats.numSolo}</h5>
                                     <h5>Number of Words Spoken: ${numTotalWords}</h5>
