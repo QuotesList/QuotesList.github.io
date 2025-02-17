43669eb948fcf460811425c92b7658b8bb733ec6
Adam Seidman <adamjseidman@gmail.com>
2025-02-17 00:08:43 -0500

    Add hamburger menu to mobile

--- js/site.js
+++ js/site.js
@@ -15,6 +15,17 @@ $(document).ready(() => {
     $('body').prepend(`
         <div class="row header justify-content-center">
             <h1 class="title">
+                <div id="hamburger-menu" class="dropdown d-inline-block dropright requires-login">
+                    <i class="fa-solid fa-bars mobile-only" id="dropdownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    </i>
+                    <div class="dropdown-menu dropdown-menu-right dropright" aria-labelledby="dropdownMenuButton">
+                        ${navItems.map(item => {
+                            return `<a class="dropdown-item bold ${item.requiresPriveleges? 'requires-admin' : ''}" href="${item.href}">${item.text}</a>`
+                        }).join('\n')}
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item bold" onclick="logOut()">Log Out</a>
+                    </div>
+                </div>
                 <a href="/">Quotes List</a>
             </h1>
         </div>
