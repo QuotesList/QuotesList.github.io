26737de0ceab63a9828a66924b3fc8f2127f8b31
Adam Seidman <adamjseidman@gmail.com>
2025-02-19 22:30:32 -0500

    Add pluralize util function

--- js/utils.js
+++ js/utils.js
@@ -42,3 +42,5 @@ const shuffleArray = (arr) => {
         [arr[currentIndex], arr[randomIndex]] = [arr[randomIndex], arr[currentIndex]]
     }
 }
+
+const pluralize = (count, word, suffix = 's') => `${word}${(count === 1)? '' : suffix}`
